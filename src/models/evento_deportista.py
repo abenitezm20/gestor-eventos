8f8694480c12c3c5854985d4ff0f5937e11fc37c
AlexB <a.benitezm@uniandes.edu.co>
2024-05-18 12:37:28 -0500

    test listar eventos

--- src/models/evento_deportista.py
+++ src/models/evento_deportista.py
@@ -8,13 +8,12 @@ from src.models.model import Model
 
 class EventoDeportista(Model, Base):
     __tablename__ = "evento_deportista"
-    id_evento = Column(UUID(as_uuid=True), ForeignKey('eventos.id'), primary_key=True)
+    id_evento = Column(UUID(as_uuid=True))
     id_deportista = Column(UUID(as_uuid=True), ForeignKey('deportista.id'), primary_key=True)
     fecha_inicio = Column(DateTime)
     fecha_fin = Column(DateTime)
 
     deportista: Mapped['Deportista'] = relationship("Deportista")
-    eventos: Mapped['Eventos'] = relationship("Eventos")
 
     def __init__(self, id_evento, id_deportista):
         Model.__init__(self)
