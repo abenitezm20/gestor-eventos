b8badfe0d282284673b0092495e684f64056c802
AlexB <a.benitezm@uniandes.edu.co>
2024-05-19 16:56:17 -0500

    #deploy test obtener eventos

--- tests/eventos/test_eventos.py
+++ tests/eventos/test_eventos.py
@@ -243,3 +243,131 @@ class TestEventos():
                 session.delete(deporte_random)
                 session.delete(deportista_random)
                 session.commit()
+
+    @patch('requests.post')
+    def test_listarEventosRegistradosTokenIncorrecto(self, mock_post):
+        with db_session() as session:
+            with app.test_client() as test_client:
+
+                # Crear Deportista
+                info_deportista = {
+                    'nombre': fake.name(),
+                    'apellido': fake.name(),
+                    'tipo_identificacion': fake.random_element(elements=(
+                        tipo_identificacion.value for tipo_identificacion in TipoIdentificacionEnum)),
+                    'numero_identificacion': fake.random_int(min=1000000, max=999999999),
+                    'email': fake.email(),
+                    'genero': fake.random_element(elements=(genero.value for genero in GeneroEnum)),
+                    'edad': fake.random_int(min=18, max=100),
+                    'peso': fake.pyfloat(3, 1, positive=True),
+                    'altura': fake.random_int(min=140, max=200),
+                    'pais_nacimiento': fake.country(),
+                    'ciudad_nacimiento': fake.city(),
+                    'pais_residencia': fake.country(),
+                    'ciudad_residencia': fake.city(),
+                    'antiguedad_residencia': fake.random_int(min=0, max=10),
+                    'contrasena': fake.password(),
+                    'deportes' : [ {"atletismo": 1}, {"ciclismo": 0}]
+                }
+                deportista_random = Deportista(**info_deportista)
+                session.add(deportista_random)
+                session.commit()
+
+                mock_response = MagicMock()
+                mock_response.status_code = 200
+                mock_response.json.return_value = {
+                    'token_valido': False, 
+                    'email': deportista_random.email,
+                    'tipo_usuario': 'deportista',
+                    'subscripcion': "Premium",
+                    }
+                mock_post.return_value = mock_response
+
+                headers = {'Authorization': 'Bearer 123'}
+                
+                response = test_client.get('/gestor-eventos/eventos/agendados', headers=headers, follow_redirects=True)
+                assert response.status_code == 401
+                session.delete(deportista_random)
+                session.commit()
+    
+    @patch('requests.post')
+    def test_listarEventosRegistradosSinEventos(self, mock_post):
+        with db_session() as session:
+            with app.test_client() as test_client:
+
+                # Crear Deportista
+                info_deportista = {
+                    'nombre': fake.name(),
+                    'apellido': fake.name(),
+                    'tipo_identificacion': fake.random_element(elements=(
+                        tipo_identificacion.value for tipo_identificacion in TipoIdentificacionEnum)),
+                    'numero_identificacion': fake.random_int(min=1000000, max=999999999),
+                    'email': fake.email(),
+                    'genero': fake.random_element(elements=(genero.value for genero in GeneroEnum)),
+                    'edad': fake.random_int(min=18, max=100),
+                    'peso': fake.pyfloat(3, 1, positive=True),
+                    'altura': fake.random_int(min=140, max=200),
+                    'pais_nacimiento': fake.country(),
+                    'ciudad_nacimiento': fake.city(),
+                    'pais_residencia': fake.country(),
+                    'ciudad_residencia': fake.city(),
+                    'antiguedad_residencia': fake.random_int(min=0, max=10),
+                    'contrasena': fake.password(),
+                    'deportes' : [ {"atletismo": 1}, {"ciclismo": 0}]
+                }
+                deportista_random = Deportista(**info_deportista)
+                session.add(deportista_random)
+                session.commit()
+
+                mock_response = MagicMock()
+                mock_response.status_code = 200
+                mock_response.json.return_value = {
+                    'token_valido': True, 
+                    'email': deportista_random.email,
+                    'tipo_usuario': 'deportista',
+                    'subscripcion': "Premium",
+                    }
+                mock_post.return_value = mock_response
+
+                headers = {'Authorization': 'Bearer 123'}
+                
+                info_deporte = {
+                    'nombre': fake.name(),
+                }
+                deporte_random = Deporte(**info_deporte)
+                session.add(deporte_random)
+                session.commit()
+                deporte_id = deporte_random.id
+                
+                # #se crea el evento
+                # info_evento = {
+                #     'id_deporte': deporte_id,
+                #     'nombre': fake.name(),
+                #     'descripcion': fake.text(),
+                #     'fecha': fake.date_time(),
+                #     'pais': fake.country(),
+                #     'lugar': fake.city()
+                # }
+                # evento_random = Eventos(**info_evento)
+                # session.add(evento_random)
+                # session.commit()
+                # evento_id = evento_random.id
+
+                # #Se asocia el evento al deportista
+                # info_evento_deportista = {
+                #     'id_deportista': deportista_random.id,
+                #     'id_evento': evento_id
+                # }
+                # evento_deportista_random = EventoDeportista(**info_evento_deportista)
+                # session.add(evento_deportista_random)
+                # session.commit()
+
+                response = test_client.get('/gestor-eventos/eventos/agendados', headers=headers, follow_redirects=True)
+                assert response.status_code == 200
+                assert response.json == []
+
+                #session.delete(evento_deportista_random)
+                #session.delete(evento_random)
+                session.delete(deporte_random)
+                session.delete(deportista_random)
+                session.commit()
